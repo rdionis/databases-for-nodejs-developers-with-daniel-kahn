aef6f719e7544dfc9d36205102f902986a233916
rdionis <dionisio.raquel@gmail.com>
2025-08-13 14:37:36 +0200

    Create, update, and delete

--- src/routes/admin/item.js
+++ src/routes/admin/item.js
@@ -12,16 +12,34 @@ export default async function (fastify) {
 
   // Route to create or edit an item
   fastify.post("/", async (request, reply) => {
-    const { itemId, sku, name, price } = request.body;
+    const { itemId, sku, name, price, tags } = request.body;
 
-    // Placeholder logic to create or update an item
-    if (itemId) {
-      fastify.log.info(`Updating item ${itemId}:`, { sku, name, price });
-    } else {
-      fastify.log.info(`Creating new item:`, { sku, name, price });
+    // parsing the tags
+    const parsedTags = tags ? tags.split(",").map((tag) => tag.trim()) : [];
+
+    // Create or update an item
+    try {
+      if (itemId) {
+        fastify.log.info(`Updating item ${itemId}:`, { sku, name, price });
+      } else {
+        await fastify.Item.create({ sku, name, price, tags: parsedTags });
+        fastify.log.info(`Created new item: ${name}`);
+      }
+      request.session.set("messages", [
+        {
+          type: "success",
+          text: itemId
+            ? "Item updated successfully"
+            : "Item created successfully"
+        }
+      ]);
+    } catch (err) {
+      request.session.set("messages", [
+        { type: "danger", text: "Failed to save item." }
+      ]);
+      fastify.log.error("Error saving item.");
+      return reply.redirect("/admin/item");
     }
-
-    return reply.redirect("/admin/item");
   });
 
   // Route to delete an item
